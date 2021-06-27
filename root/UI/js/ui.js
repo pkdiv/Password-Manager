7f44c2b6569abd662b944443467f1e00d10a101b
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-27 19:48:44 +0530

    Fixed bug in checkCred

--- root/UI/js/ui.js
+++ root/UI/js/ui.js
@@ -11,20 +11,20 @@ const filename = "..\\Password-Manager\\credentials.txt"
 // ipcRenderer.send("msg-1", 'hello')
 
 function checkCred(){
-  var logUsername = document.getElementById("user")
-  var logPassword = document.getElementById("pass")
+  var logUsername = document.getElementById("user").value
+  var logPassword = document.getElementById("pass").value
   var passHash = sha1(logPassword)
   var flag = 0;
   unpack()
 
   for(var i = 0 ; i< usernames.length; i++){
-    if (logUsername == usernames[i] && logPassword == passwords[i] ){
+    if (logUsername == usernames[i] && passHash == passwords[i] ){
       ipcRenderer.send("passwordHash", logPassword)
       flag = 1
       break
     }
   }
-  if(flag == 1){
+  if(flag == 0){
     console.log("Password not found");
   }
 }
@@ -34,8 +34,8 @@ function checkCred(){
 function unpack() {
   var buffer = fs.readFileSync(filename, 'utf-8')
   var cred = buffer.split('\n')
-  for(var record=0;record<cred.length;record++){
+  for(var record=0;record<cred.length - 1;record++){
     usernames[record] = cred[record].split('|')[0]
-    passwords[record] = cred[record].split('|')[1]
+    passwords[record] = cred[record].split('|')[1].replace(/(\r?\n)|(\r)|(\n)/g, '');
   }
 }
